1b9c0bafdedba651f57bdfc4b565437bb91d42ed
Shivam Dubey <72991053+shivampycharm@users.noreply.github.com>
2023-01-10 19:44:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+
+let textValue = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+textValue.addEventListener("input", function() {
+	
+	count.innerText = this.value.length;
+	
+})
